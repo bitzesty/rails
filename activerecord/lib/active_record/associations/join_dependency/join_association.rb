83750037d8b7bcc55d11e4da3c0026ce98a0893a
Matthew Ford <matt@bitzesty.com>
2020-10-14 13:03:36 +0100

    Backporting AR patches to fix issues with joins
    https://github.com/rails/rails/pull/29413/files

--- activerecord/lib/active_record/associations/join_dependency/join_association.rb
+++ activerecord/lib/active_record/associations/join_dependency/join_association.rb
@@ -23,13 +23,10 @@ module ActiveRecord
 
         JoinInformation = Struct.new :joins, :binds
 
-        def join_constraints(foreign_table, foreign_klass, node, join_type, tables, scope_chain, chain)
+        def join_constraints(foreign_table, foreign_klass, join_type, tables, chain)
           joins         = []
-          bind_values   = []
+          binds         = []
           tables        = tables.reverse
-
-          scope_chain_index = 0
-          scope_chain = scope_chain.reverse
 
           # The chain starts with the target table, but we want to end with it here (makes
           # more sense in this context), so we reverse
@@ -37,82 +34,24 @@ module ActiveRecord
             table = tables.shift
             klass = reflection.klass
 
-            join_keys   = reflection.join_keys(klass)
-            key         = join_keys.key
-            foreign_key = join_keys.foreign_key
-
-            constraint = build_constraint(klass, table, key, foreign_table, foreign_key)
-
-            scope_chain_items = scope_chain[scope_chain_index].map do |item|
-              if item.is_a?(Relation)
-                item
-              else
-                ActiveRecord::Relation.create(klass, table).instance_exec(node, &item)
-              end
-            end
-            scope_chain_index += 1
-
-            klass_scope =
-              if klass.current_scope && klass.current_scope.values.blank?
-                klass.unscoped
-              else
-                klass.send(:build_default_scope, ActiveRecord::Relation.create(klass, table))
-              end
-            scope_chain_items.concat [klass_scope].compact
-
-            rel = scope_chain_items.inject(scope_chain_items.shift) do |left, right|
-              left.merge right
-            end
-
-            if rel && !rel.arel.constraints.empty?
-              bind_values.concat rel.bind_values
-              constraint = constraint.and rel.arel.constraints
-            end
-
-            if reflection.type
-              value = foreign_klass.base_class.name
-              column = klass.columns_hash[reflection.type.to_s]
-
-              substitute = klass.connection.substitute_at(column)
-              bind_values.push [column, value]
-              constraint = constraint.and table[reflection.type].eq substitute
-            end
+            constraint = reflection.build_join_constraint(table, foreign_table)
 
             joins << table.create_join(table, table.create_on(constraint), join_type)
+
+            join_scope = reflection.join_scope(table, foreign_klass)
+
+            if join_scope.arel.constraints.any?
+              binds.concat join_scope.bound_attributes
+              joins.concat join_scope.arel.join_sources
+              right = joins.last.right
+              right.expr = right.expr.and(join_scope.arel.constraints)
+            end
 
             # The current table in this iteration becomes the foreign table in the next
             foreign_table, foreign_klass = table, klass
           end
 
-          JoinInformation.new joins, bind_values
-        end
-
-        #  Builds equality condition.
-        #
-        #  Example:
-        #
-        #  class Physician < ActiveRecord::Base
-        #    has_many :appointments
-        #  end
-        #
-        #  If I execute `Physician.joins(:appointments).to_a` then
-        #    klass         # => Physician
-        #    table         # => #<Arel::Table @name="appointments" ...>
-        #    key           # =>  physician_id
-        #    foreign_table # => #<Arel::Table @name="physicians" ...>
-        #    foreign_key   # => id
-        #
-        def build_constraint(klass, table, key, foreign_table, foreign_key)
-          constraint = table[key].eq(foreign_table[foreign_key])
-
-          if klass.finder_needs_type_condition?
-            constraint = table.create_and([
-              constraint,
-              klass.send(:type_condition, table)
-            ])
-          end
-
-          constraint
+          JoinInformation.new joins, binds
         end
 
         def table
